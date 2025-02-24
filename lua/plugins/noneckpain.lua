e07eb5248edd6602bf2fb93a22be5f99395aca3e
mike <mikehall@mikehall94.com>
2025-02-24 07:55:23 -0700

    had to set Lazy to false for this to work. Need to figure lazy load it in the future

--- lua/plugins/noneckpain.lua
+++ lua/plugins/noneckpain.lua
@@ -1,21 +1,22 @@
 return {
-    "shortcuts/no-neck-pain.nvim",
-    version = "*",
-    config = function()
-        require("no-neck-pain").setup({
-            buffers = {
-                scratchPad = {
-                    -- set to `false` to
-                    -- disable auto-saving
-                    enabled = true,
-                    -- set to `nil` to default 
-                    -- to current working directory
-                    location = 'nil',
-                },
-                bo = {
-                    filetype = "md"
-                },
-            },
-        })
-    end,
+  "shortcuts/no-neck-pain.nvim",
+  lazy = false,
+  version = "*",
+  config = function()
+    require("no-neck-pain").setup({
+      buffers = {
+        scratchPad = {
+          -- set to `false` to
+          -- disable auto-saving
+          enabled = true,
+          -- set to `nil` to default 
+          -- to current working directory
+          location = 'nil',
+        },
+        bo = {
+          filetype = "md"
+        },
+      },
+    })
+  end,
 }
