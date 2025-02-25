3d8249a2df38d82f2d08ce1a17b5415c7901fe07
mike <mikehall@mikehall94.com>
2025-02-24 20:07:32 -0700

    getting suggestions to work for the most part

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -6,7 +6,23 @@ return {
   opts = {
     -- add any opts here
     -- for example
-    provider = "openai",
+
+
+    provider = "claude", -- The provider used in Aider mode or in the planning phase of Cursor Planning Mode
+    -- WARNING: Since auto-suggestions are a high-frequency operation and therefore expensive,
+    -- currently designating it as `copilot` provider is dangerous because: https://github.com/yetone/avante.nvim/issues/1048
+    -- Of course, you can reduce the request frequency by increasing `suggestion.debounce`.
+    auto_suggestions_provider = "claude",
+    cursor_applying_provider = nil, -- The provider used in the applying phase of Cursor Planning Mode, defaults to nil, when nil uses Config.provider as the provider for the applying phase
+    claude = {
+      endpoint = "https://api.anthropic.com",
+      model = "claude-3-5-sonnet-20241022",
+      temperature = 0,
+      max_tokens = 4096,
+      -- api_key_name = "CLAUDE_API"
+    },
+
+
     openai = {
       endpoint = "https://api.openai.com/v1",
       model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
@@ -14,7 +30,34 @@ return {
       temperature = 0, -- adjust if needed
       max_tokens = 4096,
       -- reasoning_effort = "high" -- only supported for reasoning models (o1, etc.)
+      -- api_key_name = "OPENAI_API"
     },
+
+    suggestion = {
+        debounce = 60,
+        throttle = 600,
+    },
+
+    -- dual_boost = {
+    --   enabled = ,
+    --   first_provider = "openai",
+    --   second_provider = "claude",
+    --   prompt = "Based on the two reference outputs below, generate a response that incorporates elements from both but reflects your own judgment and unique perspective. Do not provide any explanation, just give the response directly. Reference Output 1: [{{provider1_output}}], Reference Output 2: [{{provider2_output}}]",
+    --   timeout = 60000, -- Timeout in milliseconds
+    -- },
+    behaviour = {
+      auto_suggestions = true, -- Experimental stage
+
+    --   auto_set_highlight_group = true,
+    --   auto_set_keymaps = true,
+    --   auto_apply_diff_after_generation = false,
+    --   support_paste_from_clipboard = false,
+    --   minimize_diff = true, -- Whether to remove unchanged lines when applying a code block
+    --   enable_token_counting = true, -- Whether to enable token counting. Default to true.
+    --   enable_cursor_planning_mode = false, -- Whether to enable Cursor Planning Mode. Default to false.
+    },
+
+
   },
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
   build = "make",
