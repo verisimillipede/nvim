f3cdf6816cd239eb347003299eb91b8c743a5e87
mike <mikehall@mikehall94.com>
2025-03-30 16:24:16 -0600

    follow url func

--- lua/plugins/obsidian.nvim.lua
+++ lua/plugins/obsidian.nvim.lua
@@ -34,10 +34,10 @@ return {
       ---@param url string
       follow_url_func = function(url)
         -- Open the URL in the default web browser.
-        vim.fn.jobstart({"open", url})  -- Mac OS
+        -- vim.fn.jobstart({"open", url})  -- Mac OS
         -- vim.fn.jobstart({"xdg-open", url})  -- linux
         -- vim.cmd(':silent exec "!start ' .. url .. '"') -- Windows
-        -- vim.ui.open(url) -- need Neovim 0.10.0+
+        vim.ui.open(url) -- need Neovim 0.10.0+
       end,
       disable_frontmatter = true,
       note_frontmatter_func = nil,
