cac06bb4d8ef07c058e749f962d55a9fc4ee04bc
mike <mikehall@mikehall94.com>
2025-04-13 08:44:00 -0600

    feat(markdown): Trim blanklines from end of file.

--- lua/plugins/trim.lua
+++ lua/plugins/trim.lua
@@ -7,16 +7,15 @@
 --          │                         lines.                          │
 --          └─────────────────────────────────────────────────────────┘
 
-
 return {
-    "cappyzawa/trim.nvim",
-    config = function()
-        require("trim").setup({
-            ft_blocklist = { "markdown" },
-            highlight = false,
-            trim_on_write = true,
-            trim_trailing = true,
-            trim_last_line = true,
-        })
-    end,
+  "cappyzawa/trim.nvim",
+  config = function()
+    require("trim").setup {
+      -- ft_blocklist = { "markdown" },
+      highlight = false,
+      trim_on_write = true,
+      trim_trailing = true,
+      trim_last_line = true,
+    }
+  end,
 }
